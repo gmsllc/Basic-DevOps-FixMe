f3f51a571c0fdb3ce1ef62924c56c8c8cf1cbcc3
Damian Casale <damian@dreamstarcash.com>
2012-03-15 14:50:03 +0400

    added some pretty, removed the swearing

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
+echo '
+<style>
+html {
+	text-align: center;
+}
+</style>
+';
 require_once("include.php");
 echo displayForm();
 switch (checkWord()) {
 	case 1:
-		echo "you found a word";
+		echo 'Congratulations, you found a word and by finding a word, you fixed it. <br /><img src="http://www.freemyspacegraphics.com/Graphics/Funny_Animations/images/43.gif" /><br /><img src="http://www.freemyspacegraphics.com/Graphics/Funny_Animations/images/funny_new_18.gif" width="180px;" />';
 	break;
 	case -1:
-		echo "ain't fuck all in here for you";
+		echo 'There are no words in the list that match, please try again.<br /><img src="http://www.freemyspacegraphics.com/Graphics/Funny_Animations/images/funny_2.gif"/>';
 	break;
 	default:
-		echo "no word submitted";
+		echo "Please submit a word to search for";
 	break;
 }
